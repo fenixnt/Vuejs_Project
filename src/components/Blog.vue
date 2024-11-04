e52ce014d6662021b599d8b47a6e75167efc5b5b
BinhNT <BinhNguyen0203@gmail.com>
2024-11-04 22:39:24 +0700

    [BinhNT59] Demo Vuejs and navbar

---
+++ src/components/Blog.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>This is Blog page.....</h1>
+</template>
+
+<script>
+export default {
+  name: "BlogPage",
+};
+</script>
+
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div className="container">
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <td>Họ và tên</td>
+          <td>Mail</td>
+          <td>Số điện thoại</td>
+        </tr>
+        <tr>
+          <td>Nguyễn Thái Bình</td>
+          <td>binhnguyen0203@gmail.com</td>
+          <td>09349491067</td>
+        </tr>
+        <tr>
+          <td>Nguyễn Thái Bình</td>
+          <td>binhnguyen0203@gmail.com</td>
+          <td>09349491067</td>
+        </tr>
+        <tr>
+          <td>Nguyễn Thái Bình</td>
+          <td>binhnguyen0203@gmail.com</td>
+          <td>09349491067</td>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style></style>
